3192d09ba11a67049d4839d9fe596909b8ab1d31
陈磊 <394699890@qq.com>
2021-03-08 15:28:24 +0800

    增加格式化规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,17 @@ module.exports = {
     "no-mixed-spaces-and-tabs": [0],
     "no-async-promise-executor":0,
     "no-misleading-character-class": 0,
-    "no-useless-catch": 0
+    "no-useless-catch": 0,
+    "prettier/prettier": [  
+      "error",
+      {
+        printWidth: 200,
+        singleQuote: false,
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        htmlWhitespaceSensitivity: "ignore"
+      }
+    ]
 
   },
   globals: {
